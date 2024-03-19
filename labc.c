41dfac45030e873b17d278a8edffef6f1ffb3b72
Bo Zhang <bo.zhang-17@student.manchester.ac.uk>
2024-03-20 06:42:56 +0800

    v1

--- labc.c
+++ labc.c
@@ -1,51 +1,23 @@
-#include <stdio.h> 
-
-int main(void) { 
-
- 
-
-    char grade = 'a'; 
-
-    double mark = 0; 
-
-     
-
-    switch (grade) { 
-
-    case 'A': mark = 80; 
-
-                  printf("First class"); 
-
-              break; 
-
-    case 'B': mark = 65; 
-
-                  printf("2.1"); 
-
-              break; 
-
-    case 'C': mark = 55; 
-
-                  printf("2.2"); 
-
-              break; 
-
-    case 'D': mark = 45;  
-
-                  printf("3rd"); 
-
-              break; 
-
-    case 'E': mark = 35; 
-
-                  printf("Failing"); 
-
-              break; 
-
-    default: printf("Error. Something went wrong"); 
-
-             break; 
-
-    } 
-
-} 
+#include <stdio.h>
+int main(void){
+    char my_email[]="bo.zhang-17@student.manchester.ac.uk";
+    int n=0;
+	printf("Enter an integer:");
+	scanf("%d", &n);
+	
+	
+	// Your code below here
+    if(my_email[n]=='a')
+	{printf("Vowel"); }// place as needed in the code. You can use more than once.
+	else if(my_email[n]=='e')
+	{printf("Vowel"); }
+    else if(my_email[n]=='i')
+	{printf("Vowel"); }
+    else if(my_email[n]=='o')
+	{printf("Vowel"); }
+    else if(my_email[n]=='u')
+	{printf("Vowel"); }
+    else{printf("Not vowel"); }
+	// Do not edit below here
+	return 0;
+}
